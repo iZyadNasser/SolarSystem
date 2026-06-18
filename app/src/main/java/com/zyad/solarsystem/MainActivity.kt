f32a884c3c0f116b8c9f57a8337ec9d4b36541e3
Zyad <zyadhammad531@gmail.com>
2026-06-18 21:42:23 +0300

    feat: add main screen and background

--- app/src/main/java/com/zyad/solarsystem/MainActivity.kt
+++ app/src/main/java/com/zyad/solarsystem/MainActivity.kt
@@ -7,18 +7,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.IntRange
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            PlanetScreen()
         }
     }
+}
+
+@Composable
+fun PlanetScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.im_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewPlanetScreen() {
+    PlanetScreen()
 }
 
 // Font Family
